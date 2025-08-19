c6c7a8514e56f0a6df0a7ed29d0d0f09ee201ea4
Sofia Palomino <sofiapalominochamizo@gmail.com>
2025-08-19 14:54:15 +0900

    Handle users

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from typing import Optional, List
-import psycopg2
 from psycopg2.extras import RealDictCursor
 from random import randrange
 from sqlalchemy.orm import Session
-import time
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, SessionLocal, get_db
+
+import psycopg2
+import time
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -128,3 +129,28 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
 
     return post_query.first
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    #hash the password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    
+    new_user = models.User(**user.dict())
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id} does not exist")
+
+    return user
--- app/models.py
+++ app/models.py
@@ -12,3 +12,11 @@ class Post(Base):
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default = 'TRUE', nullable = False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = "users"
+
+    email =  Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    id = Column(Integer, primary_key = True, nullable = False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
