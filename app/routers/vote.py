615d994466e4b80c1dbec71b6b69559b66305e71
Sofia Palomino <sofiapalominochamizo@gmail.com>
2025-08-27 16:15:17 +0900

    fix bugs

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -15,6 +15,11 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id).first()
     found_vote = vote_query.first()
 
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {vote.post_id} does not exist")
+
     if (vote.dir == 1):
         if found_vote:
            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
